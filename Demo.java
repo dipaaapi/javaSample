ba5618d6920d6d9be9db146d3e6340088dbf107e
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-02-13 02:05:36 +0800

    data types added

--- Demo.java
+++ Demo.java
@@ -1,12 +1,20 @@
 public class Demo {
     public static void main(String[] args) {
         String nname, name, fname, lname;
-        fname = "john edward";
-        lname = "cabrera";
-        nname = "Ed's";
+        int age;
+
+        fname = "john edward"; lname = "cabrera"; nname = "Ed's";
         name = fname + " " + lname;
+
+        age = 33;
+
+        String[] plang = {"html", "php", "java"};
+
         System.out.println( nname + " first Java program!"); //first line to display
         System.out.println(name + "'s next Goal is to become Java Developer");
         System.out.println("Eat | Sleep | Code == Never stop && Focus on your Goals");
+        System.out.println("I'm " + age + " years old");
+        System.out.println("I know " + plang.length + " programming language");
+        System.out.println("these are " + plang[0] + " " + plang[1] + " " + plang[2]);
     }
 }
