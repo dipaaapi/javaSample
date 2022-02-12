a36d257a9fa1f5dac93406c2dadc6dd7426dac59
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-02-13 04:25:54 +0800

    added math method

--- Demo.java
+++ Demo.java
@@ -1,21 +1,24 @@
 public class Demo {
     public static void main(String[] args) {
         String nickName, name, fname, lname, knowledge;
-        int age;
+        int age, techStack, uidesign, plang, max, min;
         boolean dev, x;
         double percent;
 
         fname = "john edward"; lname = "cabrera"; nickName = "ed"; knowledge = "Is he knowledgeable in programming? the answer is ";
         name = fname + " " + lname;
 
-        age = 33;
-
         percent = 100.00;
-
+        
         String[] programmingLanguage = {"TypeScript", "JavaSript", "php", "java"};
         String[] uid = {"adobe xd", "balsamiq", "figma"};
+        
+        age = 33; uidesign = uid.length; plang = programmingLanguage.length;
+        dev = true; x = uidesign<plang;
+        techStack = uid.length + programmingLanguage.length;
 
-        dev = true; x = uid.length<programmingLanguage.length;
+        max = Math.max(uidesign, plang);
+        min = Math.min(uidesign, plang);
 
         System.out.println( nickName + "'s first Java program!"); //first line to display
         System.out.println(name + "'s next Goal is to become Java Developer");
@@ -29,6 +32,11 @@ public class Demo {
         System.out.println("The answer is " + dev + " he is " + percent + "% Dev.");
         System.out.println("In total knowledge in programming are " + programmingLanguage.length + " and in UI Design " + uid.length);
         System.out.println(knowledge + " " + x);
-        System.out.println(knowledge.indexOf("programming") + " is the age where I started my dream to become an I.T.");
+        System.out.println(knowledge.indexOf("programming") + " is the age where I started \nmy dream to become an \tI.T.");
+        System.out.println("though my techstack in UI Designing is " + min);
+        System.out.println("but my techstack in programming language is " + max);
+        System.out.println("that is why when I start my carrier in tech industry I gain " + techStack + " total in techstacks");
+        System.out.println("The square root of ("+ techStack + ") techstack is " + Math.sqrt(techStack) + " I'm just saying hahaha");
+        System.out.println("At least the number of techstack that I need to learn is " + (int)Math.random()*2);
     }
 }
