9cd687ef4b0d3adef62d6ab185125041d79d07a5
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-02-13 01:32:04 +0800

    initial commit
    this is for learning new language thank you.

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+        String nname, name, fname, lname;
+        fname = "john edward";
+        lname = "cabrera";
+        nname = "Ed's";
+        name = fname + " " + lname;
+        System.out.println( nname + " first Java program!"); //first line to display
+        System.out.println(name + "'s next Goal is to become Java Developer");
+        System.out.println("Eat | Sleep | Code == Never stop && Focus on your Goals");
+    }
+}
